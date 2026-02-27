56cae87fc6ffe308ab1d143ce941d5dafc55f1fe
baklan11223-7 <mishchuk2211@gmail.com>
2026-02-27 20:34:20 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -129,9 +129,7 @@ def handle(message):
         total_month = 0
 
         for shift in user_data[chat_id]["shifts"]:
-            shift_date = datetime.now()
-
-strptime(shift["date"], "%Y-%m-%d")
+            shift_date = datetime.strptime(shift["date"], "%Y-%m-%d")
             if shift_date.month == month and shift_date.year == year:
                 total_month += shift["total"]
 
