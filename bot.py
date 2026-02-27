beb75fc9b56433dbe1897fc2a94a0cea3a0b9048
baklan11223-7 <mishchuk2211@gmail.com>
2026-02-28 00:43:18 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -74,7 +74,6 @@ def handle(message):
         bot.send_message(chat_id, "Введіть день місяця (1–31):")
         return
 
-    if step == "change_day":
 if step == "change_day":
     try:
         day = int(text)
@@ -87,7 +86,7 @@ if step == "change_day":
                 f"День змінено на {day} число ✅",
                 reply_markup=main_keyboard()
             )
-        else:
+         else:
             bot.send_message(chat_id, "Введіть число від (1 до 31):")
 
     except Exception:
