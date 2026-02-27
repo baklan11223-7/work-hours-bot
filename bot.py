146542ff331a456aab5b0151bc6e240dfe3bc59f
baklan11223-7 <mishchuk2211@gmail.com>
2026-02-27 18:45:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -256,5 +256,5 @@ def webhook():
 def index():
     return "Bot is running"
 
-if name == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
