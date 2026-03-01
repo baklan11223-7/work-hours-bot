9631b2686f5bb3098d6307eb93f73a60d1a9203d
baklan11223-7 <mishchuk2211@gmail.com>
2026-03-02 00:42:38 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ bot = telebot.TeleBot(TOKEN)
 app = Flask(__name__)
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-conn = psycopg2.connect(DATABASE_URL, sslmode="reqyire")
+conn = psycopg2.connect(DATABASE_URL, sslmode="require")
 cursor = conn.cursor()
 
 positions = {
